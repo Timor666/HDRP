1ed1fe102429f741850f29b0ef9855d34a88994c
Timor666 <123456>
2020-07-07 17:46:50 +0800

    首次提交HDRP

---
+++ Assets/Editor/BakedTimerWindow.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System;
+using System.Threading;
+
+public class BakedTimerWindow : EditorWindow
+{
+    public bool isBakeStart = false;
+    public bool isBakeUpadate = false;
+    public bool isBakeCompleted = false;
+
+    public System.DateTime bakeStartTime;
+    public System.DateTime bakeCompletedTime;
+    public void Awake()
+    {
+        Lightmapping.bakeStarted += OnBakeStarted;
+        Lightmapping.lightingDataUpdated += OnLightingDataUpdated;
+        Lightmapping.bakeCompleted += OnBakeCompleted;
+    }
+
+    public void OnDestroy()
+    {
+        Lightmapping.bakeStarted -= OnBakeStarted;
+        Lightmapping.lightingDataUpdated -= OnLightingDataUpdated;
+        Lightmapping.bakeCompleted -= OnBakeCompleted;
+    }
+
+    public void OnGUI()
+    {
+        if (isBakeStart)
+        {
+            GUILayout.Label("开始时间: " +bakeStartTime, EditorStyles.boldLabel);
+        }
+
+        if (isBakeCompleted)
+        {
+            GUILayout.Label("--------------------------------------------------------");
+            GUILayout.Label("开始时间: " + bakeStartTime, EditorStyles.boldLabel);
+            GUILayout.Label("结束时间: " + bakeCompletedTime.ToString(), EditorStyles.boldLabel);
+
+            TimeSpan time = bakeCompletedTime - bakeStartTime;
+            GUILayout.Label("花费时间: " + time.ToString(), EditorStyles.boldLabel);
+        }
+
+        if (isBakeUpadate && Lightmapping.isRunning)
+        {
+            TimeSpan time = DateTime.Now - bakeStartTime;
+            GUILayout.Label("花费时间: " +  time.ToString(), EditorStyles.boldLabel);
+        }
+    }
+
+    public void OnInspectorUpdate()
+    {
+        Repaint();
+    }
+
+    [MenuItem("Timor/Window/BakedTimer")]
+    public static void ShowWindow()
+    {
+        BakedTimerWindow window = GetWindow<BakedTimerWindow>("烘焙计时");
+    }
+    public void OnBakeStarted()
+    {
+        isBakeStart = true;
+        bakeStartTime = DateTime.Now;
+        isBakeCompleted = false;
+    }
+    public void OnLightingDataUpdated()
+    {
+        isBakeUpadate = true;
+    }
+    public void OnBakeCompleted()
+    {
+        isBakeCompleted = true;
+        bakeCompletedTime = DateTime.Now;
+
+        isBakeStart = false;
+        isBakeUpadate = false;
+    }
+}
---
+++ Assets/Scripts/ApplicationQuit.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class ApplicationQuit : MonoBehaviour
+{
+    public void OnQuit()
+    {
+#if UNITY_EDITOR
+        if (Application.isPlaying && Application.isEditor)
+        {
+            EditorApplication.isPlaying = false;
+        }
+#endif
+
+        Application.Quit();
+    }
+}
